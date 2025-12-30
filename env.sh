b3e58d59b350126b85b90cbe1e7e654ad4ae944d
mudassir2222 <mudassirkhan376@gmail.com>
2025-12-30 12:01:49 +0530

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
 export JAVA_HOME=/usr/lib/jvm/java-17-openjdk
 export PATH=$JAVA_HOME/bin:$PATH
 
-export IMAGE_NAME=mudassir376/onlinebookstore
+export IMAGE_NAME=mudassir376/bookservice
 export IMAGE_TAG=latest
-
 export SONAR_HOST_URL=http://192.168.220.128:9000
