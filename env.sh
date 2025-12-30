ecd1c728bc79f4c110b1be4b65c127dc3cfb5cf5
mudassir2222 <mudassirkhan376@gmail.com>
2025-12-30 12:31:30 +0530

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# ======================
-# Application Environment
-# ======================
+# =================================
+# Common Environment Configuration
+# =================================
 
 # ---------- Java ----------
 JAVA_HOME=/usr/lib/jvm/java-17-openjdk
@@ -9,10 +9,28 @@ PATH="$JAVA_HOME/bin:$PATH"
 export JAVA_HOME PATH
 
 # ---------- Docker ----------
-IMAGE_NAME="mudassir376/bookservice"
-IMAGE_TAG="latest"
-export IMAGE_NAME IMAGE_TAG
+IMAGE_NAME="mudassir376/onlinebookstore"
+export IMAGE_NAME
 
 # ---------- SonarQube ----------
 SONAR_HOST_URL="http://192.168.220.128:9000"
 export SONAR_HOST_URL
+
+# ---------- AWS / ECR ----------
+AWS_REGION="ap-south-1"
+AWS_ACCOUNT_ID="282131004193"
+ECR_REPO="jenkinsrepo"
+export AWS_REGION AWS_ACCOUNT_ID ECR_REPO
+
+# ---------- ECS ----------
+ECS_CLUSTER="ecs-jenkins-clusterr"
+ECS_SERVICE="jenkins-bookservice-task-service"
+export ECS_CLUSTER ECS_SERVICE
+
+# ---------- Logging ----------
+LOG_DIR="/var/lib/jenkins/Consolelogs"
+export LOG_DIR
+
+# ---------- Notifications ----------
+APPROVER_EMAIL="mudassirkhan376@gmail.com"
+export APPROVER_EMAIL
